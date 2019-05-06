805e1c3922e9d6381ba7dfb9229468b0f4092fe2
Vinny Bass <vinicius@starste.com>
2019-05-06 14:03:12 -0300

    fix redirect to aws

--- client/js/constants.js
+++ client/js/constants.js
@@ -1,5 +1,5 @@
-const APP = "http://localhost/chat/client/"
-const SERVER = "http://localhost/chat/server/api/src/"
+const APP = "3.14.105.238/chat/client/"
+const SERVER = "3.14.105.238/chat/server/api/src/"
 const USER_KEY = '__uid'
 
 const MIXIN = {
